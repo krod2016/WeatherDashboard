0c96c2b84c2d18cb6c8cc6db1b9ac455351adfba
krod2016 <katrinajrodriguez1993@gmail.com>
2022-01-27 20:27:41 -0600

    fixed API call

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,7 @@ const currentWeather = document.querySelector('#currentWeather');
 const searchList = document.querySelector('#searchList');
 const forecastWeather = document.querySelector('#fiveDay');
 const submitButton = document.querySelector('#submit');
-let lat;
-let lon;
+const openKey = 'ee7cf386229176507f4fbdf87aa25e5f';
 let searchArray = []
 
 //recent searches
@@ -39,7 +38,7 @@ const recentSearchHandler = function (event) {
 
   const location = weatherInput.value.trim();
   if (location) {
-    getWeather(location);
+    getWeather(location); 
   }
 
   saveSearch(location);
@@ -68,158 +67,86 @@ const saveSearch = function (searchTerm) {
 }
 
 //gives location latitude and longitude
-const getWeather = function (location) {
-  fetch(`https://dev.virtualearth.net/REST/v1/Locations?query=${location}&key=AlHqyYU_KwIAYUdgDdxmuZEdZI1CciCfJvl8u33LuVbx_bhnP1oMEp7hoqw7UIhF`)
-  .then(function (response) {
-    response.json()
-    .then(function() {
-      const newVariable = fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=ee7cf386229176507f4fbdf87aa25e5f`)
-      return newVariable
-    })
-    .then(function (result) {
-      console.log(result)
-    });
-  });
+const getWeather = async function (location) {
+  const weatherRes = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${openKey}`)
+  const weatherObj = await weatherRes.json()
+  const {lat , lon} = weatherObj.coord
+  console.log("Weather Log: ", weatherObj);
+
+  const forecastRes = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${openKey}`)
+  const forecastObj = await forecastRes.json()
+  console.log("Forcast Log: ", forecastObj);
+  
+  getForecast(forecastObj)
+
 };
 
-      //gets city's weather
-      // const weatherAPI = function() {
-      // fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=ee7cf386229176507f4fbdf87aa25e5f`) 
-      // }
-      // //         const data = weatherAPI(function (response) {
-      //           response.json().then(function (dataKey) {
-      //             const temp = document.createElement("h1");
-      //             temp.textContent = `${Math.round(dataKey.current.temp)}°F`;
+// Get the 5 day forecast
+const getForecast = function (data) {
+  console.log(data)
+  forecastWeather.textContent = '';
 
-      //             const locationText = document.createElement("h3");
-      //             locationText.textContent = location;
-      //             locationText.classList.add("card-text");
-      //             locationText.classList.add("capitalize");
+  data.daily.forEach(function (value, index) {
+    if (index > 0 && index < 6) {
+      const forecastContainer = document.createElement("div");
+      forecastContainer.classList.add("text-center");
 
-      //             const today = document.createElement("h3");
-      //             today.textContent = "Today";
-      //             today.classList.add("card-text", "-mb-2");
+      const forecastDate = document.createElement("p");
+      const unixDate = value.dt;
+      forecastDate.textContent = new Date(unixDate * 1000).toLocaleString('en-US', { weekday: 'long' });
+      forecastDate.classList.add('pb-14');
 
-      //             const windSpeed = document.createElement("p");
-      //             windSpeed.textContent = `Wind: ${Math.round(dataKey.current.wind_speed)} MPH`;
-      //             windSpeed.classList.add("card-text");
+      const forecastTemp = document.createElement("h2");
+      forecastTemp.textContent = `${Math.round(value.temp.day)}°F`;
+      forecastTemp.classList.add('pb-14');
 
-      //             const humidity = document.createElement("p");
-      //             humidity.textContent = `Humidity: ${dataKey.current.humidity}%`;
-      //             humidity.classList.add("card-text");
+      const forecastWindSpeed = document.createElement("p");
+      forecastWindSpeed.textContent = `${Math.round(value.wind_speed)} MPH`;
 
-      //             const uvIndexContainer = document.createElement("div");
-      //             const uvIndex = document.createElement("p");
-      //             const uvRating = document.createElement("span");
-      //             uvIndex.textContent = dataKey.current.uvi;
-      //             uvIndex.classList.add("card-text");
-      //             uvRating.classList.add("card-text");
+      const forecastHumidity = document.createElement("p");
+      forecastHumidity.textContent = `${value.humidity}%`;
 
-      //             uvIndexContainer.appendChild(uvIndex);
-      //             uvIndex.appendChild(uvRating);
+      const forecastIcon = document.createElement("img");
+      const forecastIconId = value.weather[0].icon;
+      forecastIcon.setAttribute('src', '');
+      forecastIcon.setAttribute('alt', 'Weather icon');
+      forecastIcon.classList.add('pb-20');
 
-      //             const icon = document.createElement("img");
-      //             const iconId = dataKey.current.weather[0].icon;
-      //             icon.setAttribute('src', '');
-      //             icon.setAttribute('alt', 'Weather icon');
+      forecastContainer.appendChild(forecastDate);
+      forecastContainer.appendChild(forecastIcon);
+      forecastContainer.appendChild(forecastTemp);
+      forecastContainer.appendChild(forecastWindSpeed);
+      forecastContainer.appendChild(forecastHumidity);
 
-      //             currentWeather.appendChild(icon);
-      //             currentWeather.appendChild(temp);
-      //             currentWeather.appendChild(today);
-      //             currentWeather.appendChild(locationText);
-      //             currentWeather.appendChild(uvIndexContainer);
+      forecastWeather.appendChild(forecastContainer);
+    }
+  });
+}
 
-      //             currentWeather.appendChild(humidity);
-      //             currentWeather.appendChild(windSpeed);
+submitButton.addEventListener('click', formSubmitHandler);
+createRecentSearches();
+weatherInput.addEventListener('click', function (e) {
+  searchList.classList.toggle("show");
+});
 
-      //             if (dataKey.current.uvi < 3) {
-      //               uvIndex.classList.add('uv-low');
-      //               uvRating.textContent = " Good"
-      //             } else if (dataKey.current.uvi >= 3 && dataKey.current.uvi < 8) {
-      //               uvIndex.classList.add('uv-medium');
-      //               uvRating.textContent = " Medium"
-      //             } else {
-      //               uvIndex.classList.add('uv-high');
-      //               uvRating.textContent = " High"
-      //             }
-      //             getForecast(dataKey);
-      //           })
+window.onclick = function (event) {
+  if (!event.target.matches('#weatherInput') && !event.target.matches('#searchList')) {
+    // searchList.classList.remove("show");
+  }
+}
 
+window.onload = function () {
+  createRecentSearches();
 
-      //           //clears out previous searches
-      //           // currentWeather.textContent = '';
-      //           // console.log(latitude);
-      //           // console.log(longitude);
-      //         });
-      //       });
-      //   };
+  // Get the most recent location from localstorage
+  if (localStorage.getItem('searchItem')) {
+    searchArray = JSON.parse(localStorage.getItem('searchItem'));
+    searchArray.reverse();
+  } else {
+    searchArray = [""];
+  }
 
-      // Get the 5 day forecast
-      const getForecast = function (data) {
-        console.log(data)
-        forecastWeather.textContent = '';
+  getWeather(searchArray[0]);
 
-        data.daily.forEach(function (value, index) {
-          if (index > 0 && index < 6) {
-            const forecastContainer = document.createElement("div");
-            forecastContainer.classList.add("text-center");
-
-            const forecastDate = document.createElement("p");
-            const unixDate = value.dt;
-            forecastDate.textContent = new Date(unixDate * 1000).toLocaleString('en-US', { weekday: 'long' });
-            forecastDate.classList.add('pb-14');
-
-            const forecastTemp = document.createElement("h2");
-            forecastTemp.textContent = `${Math.round(value.temp.day)}°F`;
-            forecastTemp.classList.add('pb-14');
-
-            const forecastWindSpeed = document.createElement("p");
-            forecastWindSpeed.textContent = `${Math.round(value.wind_speed)} MPH`;
-
-            const forecastHumidity = document.createElement("p");
-            forecastHumidity.textContent = `${value.humidity}%`;
-
-            const forecastIcon = document.createElement("img");
-            const forecastIconId = value.weather[0].icon;
-            forecastIcon.setAttribute('src', '');
-            forecastIcon.setAttribute('alt', 'Weather icon');
-            forecastIcon.classList.add('pb-20');
-
-            forecastContainer.appendChild(forecastDate);
-            forecastContainer.appendChild(forecastIcon);
-            forecastContainer.appendChild(forecastTemp);
-            forecastContainer.appendChild(forecastWindSpeed);
-            forecastContainer.appendChild(forecastHumidity);
-
-            forecastWeather.appendChild(forecastContainer);
-          }
-        });
-      }
-
-      submitButton.addEventListener('click', formSubmitHandler);
-      createRecentSearches();
-      weatherInput.addEventListener('click', function (e) {
-        searchList.classList.toggle("show");
-      });
-
-      window.onclick = function (event) {
-        if (!event.target.matches('#weatherInput') && !event.target.matches('#searchList')) {
-          // searchList.classList.remove("show");
-        }
-      }
-
-      window.onload = function () {
-        createRecentSearches();
-
-        // Get the most recent location from localstorage
-        if (localStorage.getItem('searchItem')) {
-          searchArray = JSON.parse(localStorage.getItem('searchItem'));
-          searchArray.reverse();
-        } else {
-          searchArray = [""];
-        }
-
-        getWeather(searchArray[0]);
-
-        weatherInput.value = searchArray[0];
-      };
+  weatherInput.value = searchArray[0];
+};
